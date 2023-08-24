afe252dba2b2aca85aee2e40b8be06ccc5410d4b
ycl818 <kevinyichienlee@gmail.com>
2023-08-24 14:20:43 +0800

    add section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Services,
+  Footer,
+} from "./sections";
+
 const App = () => {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffer</section>
-      <section className="bg-pale-blue padding">CustomerReviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="bg-balck padding-x padding-t pb-8">Footer</section>
+      {/* nav */}
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-balck padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };
